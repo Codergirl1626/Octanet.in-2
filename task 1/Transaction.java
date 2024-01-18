b58e33eb6a32255a55a717a652521a8d532081ca
Codergirl1626 <150144127+Codergirl1626@users.noreply.github.com>
2024-01-18 12:20:48 +0530

    ATMInterface

---
+++ task 1/Transaction.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Transaction{
+    private double amount;//amount of this trasaction
+    private Date timestamp;//time and date of this transcation 
+    private String memo;//memo for this transaction
+    public Transaction(double amount,Account inAccount){
+        this.amount = amount;
+        this.timestamp = new Date();
+        this.memo = "";
+    }
+    public Transaction(double amount,String memo,Account inAccount){
+        //call two args constructor first
+        this(amount,inAccount);
+        //set the memo
+        this.memo = memo;
+    }
+    public double getAmount(){
+        return this.amount;
+    }
+    public String getSummaryLine(){
+        if(this.amount >= 0){
+            return String.format(this.timestamp.toString()+" : $"+this.amount+" : "+this.memo);
+        }else{
+            return String.format(this.timestamp.toString()+" : $("+this.amount+") : "+this.memo);
+        }
+    }
+
+}
